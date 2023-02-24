1db6c9c71d5d3e6fe185de6d3773df7f44818649
Alexander <negreba.sanya@yandex.ru>
2023-02-25 00:17:45 +0700

    Реализивано создание разделов и проверка на их наличие. Добавлено меню. Логика чекбоксов не доведена до рабочего состояния.

--- reginfoeduorg.php
+++ reginfoeduorg.php
@@ -8,48 +8,196 @@ License: GPLv2 or later
 Text Domain: reginfoeduorg
 */
 
-if(!defined('ABSPATH')){
+if (!defined('ABSPATH')) {
     die;
 }
 
 class RegInfoEduOrg
 {
-  function __construct() {
-    add_action( 'init', array( $this, 'my_plugin_add_sections' ) );
-    add_filter( 'wp_nav_menu_items', array( $this, 'my_nav_menu_items' ), 10, 2 );
-  }
-
- function my_plugin_add_sections() {
-  // проверяем, существует ли страница с заголовком "New Page"
-  $page_id = get_page_by_title( 'New Page' );
-
-  // если страницы не существует, создаем новую страницу
-  if ( !$page_id ) {
-    $page_data = array(
-      'post_title'    => 'New Page',
-      'post_content'  => 'This is my new page!',
-      'post_status'   => 'publish',
-      'post_author'   => 1,
-      'post_type'     => 'page',
-    );
-    $page_id = wp_insert_post( $page_data );
-  }
-}
-
-  function my_nav_menu_items( $items, $args ) {
-    $page_id = get_page_by_title( 'New Page' );
-
-    // добавляем ссылку на раздел в меню сайта, только если страницы не существует
-    if ( !$page_id ) {
-      return $items;
+    function __construct() 
+    {
+        add_action('init', array($this, 'my_plugin_add_sections'));
+        add_action('admin_menu', array($this, 'my_plugin_settings_menu'));
+        add_action('admin_init', array($this, 'my_plugin_settings_init'));
+        add_action('admin_menu', array($this, 'add_admin_menu'));
     }
 
-    $new_item = '<li><a href="' . get_permalink( $page_id ) . '">New Page</a></li>';
+    function print_sections_info() 
+    {
+        echo 'Выберите страницы, которые будут содержать информацию о вашей организации:';
+    }
 
-    return $items . $new_item;
-  }
-}
+    function my_plugin_add_sections() 
+    {
 
-if(class_exists('RegInfoEduOrg')){
+        // Проверяем, существует ли страница "Сведения об образовательной организации"
+        $parent_page_id = get_page_by_title( 'Сведения об образовательной организации' );
+
+        // Если страницы не существует, создаем новую страницу
+        if ( ! $parent_page_id ) 
+        {
+            $page_data = array(
+                'post_title'    => 'Сведения об образовательной организации',
+                'post_content'  => '',
+                'post_status'   => 'publish',
+                'post_author'   => 1,
+                'post_type'     => 'page',
+            );
+            $parent_page_id = wp_insert_post( $page_data );
+        }
+
+        // Создаем подразделы на странице "Сведения об образовательной организации"
+        $pages = array(
+            'Основные сведения',
+            'Структура и органы управления образовательной организацией',
+            'Документы',
+            'Образование',
+            'Образовательные стандарты',
+            'Руководство. Педагогический (научно-педагогический) состав',
+            'Материально-техническое обеспечение и оснащенность образовательного процесса',
+            'Стипендии и иные виды материальной поддержки',
+            'Платные образовательные услуги',
+            'Финансово-хозяйственная деятельность',
+            'Вакантные места для приема (перевода)',
+        );
+
+        foreach ( $pages as $page ) 
+        {
+            // проверяем, есть ли на сайте необходимые разделы и если их нет, добавляем
+            if ( ! get_page_by_title( $page, 'OBJECT', 'page' ) ) 
+            {
+                $page_id = wp_insert_post( array(
+                    'post_title' => $page,
+                    'post_content' => '',
+                    'post_type' => 'page',
+                    'post_parent' => $parent_page_id,
+                    'post_status' => 'publish'
+                ) );
+            }
+        }  
+    }          
+
+    function my_plugin_settings_menu() 
+    {
+        add_options_page('RegInfoEduOrg Settings', 'RegInfoEduOrg', 'manage_options', 'reginfoeduorg_settings', array($this, 'my_plugin_settings_page'));
+    }
+
+    function my_plugin_settings_init() 
+    {
+        add_settings_section( 'reginfoeduorg_sections', 'Настройки RegInfoEduOrg', array($this, 'print_sections_info'), 'reginfoeduorg' );
+        add_settings_field( 'reginfoeduorg_pages', 'Выберите страницы', array($this, 'print_sections_input'), 'reginfoeduorg', 'reginfoeduorg_sections' );
+        register_setting( 'reginfoeduorg_options', 'reginfoeduorg_options', 'my_plugin_options_validate' );
+    }
+
+    function my_plugin_options_validate( $input ) {
+        // Получаем список всех страниц
+        $all_pages = get_pages(array('post_type' => 'page'));
+
+        // Получаем список страниц, которые нужно оставить
+        $selected_pages = array();
+        if (!empty($input)) {
+            foreach ($input as $key => $value) {
+                if ($value) {
+                    $selected_pages[] = $key;
+                }
+            }
+        }
+
+        // Удаляем страницы, которые не выбраны в настройках плагина
+        foreach ($all_pages as $page) {
+            if (!in_array($page->post_title, $selected_pages)) {
+                wp_delete_post($page->ID, true);
+            }
+        }
+
+        // Создаем или обновляем страницы, которые были отмечены
+        foreach ( $input as $key => $value ) {
+            if ( $value == 1 ) {
+                if ( get_page_by_title( $key, 'OBJECT', 'page' ) ) {
+                    // Страница уже существует, обновляем ее
+                    $page = get_page_by_title( $key );
+                    $my_post = array(
+                        'ID' => $page->ID,
+                        'post_content' => '',
+                        'post_title' => $key,
+                        'post_status' => 'publish',
+                        'post_type' => 'page',
+                    );
+                    wp_update_post( $my_post );
+                } else {
+                    // Страница не существует, создаем ее
+                    $my_post = array(
+                        'post_title' => $key,
+                        'post_content' => '',
+                        'post_status' => 'publish',
+                        'post_author' => 1,
+                        'post_type' => 'page',
+                    );
+                    wp_insert_post( $my_post );
+                }
+            }
+        }
+
+        return $input;
+    }
+
+    function my_plugin_settings_page() 
+    {
+        ?>
+        <div class="wrap">
+            <h1>Настройки RegInfoEduOrg</h1>
+            <form method="post" action="options.php">
+                <?php
+                settings_fields( 'reginfoeduorg_options' );
+                do_settings_sections( 'reginfoeduorg' );
+                submit_button();
+                ?>
+            </form>
+        </div>
+        <?php
+    }
+
+    function print_sections_input() 
+    {
+        $options = get_option( 'reginfoeduorg_options' );
+        $pages = array(
+            'Основные сведения',
+            'Структура и органы управления образовательной организацией',
+            'Документы',
+            'Образование',
+            'Образовательные стандарты',
+            'Руководство. Педагогический (научно-педагогический) состав',
+            'Материально-техническое обеспечение и оснащенность образовательного процесса',
+            'Стипендии и иные виды материальной поддержки',
+            'Платные образовательные услуги',
+            'Финансово-хозяйственная деятельность',
+            'Вакантные места для приема (перевода)',
+        );
+        foreach ( $pages as $page ) 
+        {
+            $checked = '';
+            if ( isset( $options[ $page ] ) ) 
+            {
+                $checked = 'checked';
+            }
+            echo '<label><input type="checkbox" name="reginfoeduorg_options[' . $page . ']" value="1" ' . $checked . ' /> ' . $page . '</label><br />';
+        }
+    }
+  
+    function add_admin_menu() 
+    {
+        add_menu_page(
+            'RegInfoEduOrg',
+            'RegInfoEduOrg',
+            'manage_options',
+            'reginfoeduorg',
+            array( $this, 'my_plugin_settings_page' )
+        );
+    }
+
+} 
+
+if(class_exists('RegInfoEduOrg'))
+{
     new RegInfoEduOrg();
 }
